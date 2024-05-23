f2a6c244ab0e623a7e56db50b159fbf8dbd3266b
arthcarrigan <arnaouthisham@gmail.com>
2024-05-23 16:24:21 +0100

    Create chatbot.js

---
+++ chatbot.js
@@ -0,0 +1,20 @@
+    <script>
+
+!function (w, d, s) {
+  var div = d.createElement('div');
+  div.id = 'aichatbot';
+  d.body.appendChild(div);
+  for (var _len = arguments.length, args = new Array(_len > 3 ? _len - 3 : 0), _key = 3; _key < _len; _key++) {
+    args[_key - 3] = arguments[_key];
+  }
+  w.chatbotConfig = args;
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s);
+  j.defer = true;
+  j.type = 'module';
+  j.src = 'https://aichatbot.sendbird.com/index.js';
+  f.parentNode.insertBefore(j, f);
+}(window, document, 'script', '65A04B71-880D-4431-BFA0-E7E483874691', 'onboarding_bot', {
+  apiHost: 'https://api-cf-eu-1.sendbird.com'
+});
+</script>
